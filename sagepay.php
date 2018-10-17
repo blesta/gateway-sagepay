c3160212b73cba7b3e5d656a868d6f6f6cf17128
Temporary <Temporary@Paul-XPS>
2018-10-17 14:06:55 -0700

    Added license file Changed the name of the readme file fixed the composer file Moved info to a config file

--- sagepay.php
+++ sagepay.php
@@ -15,16 +15,6 @@
 class Sagepay extends MerchantGateway implements MerchantCc
 {
     /**
-     * @var string The version of this gateway
-     */
-    private static $version = '1.0.0';
-
-    /**
-     * @var string The authors of this gateway
-     */
-    private static $authors = [['name' => 'Phillips Data, Inc.', 'url' => 'http://www.blesta.com']];
-
-    /**
      * @var array An array of meta data for this gateway
      */
     private $meta;
@@ -52,6 +42,8 @@ class Sagepay extends MerchantGateway implements MerchantCc
      */
     public function __construct()
     {
+        $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
+
         // Load components required by this module
         Loader::loadComponents($this, ['Input']);
 
@@ -73,46 +65,6 @@ class Sagepay extends MerchantGateway implements MerchantCc
             ];
             $this->Input->setErrors($errors);
         }
-    }
-
-    /**
-     * Returns the name of this gateway.
-     *
-     * @return string The common name of this gateway
-     */
-    public function getName()
-    {
-        return Language::_('Sagepay.name', true);
-    }
-
-    /**
-     * Returns the version of this gateway.
-     *
-     * @return string The current version of this gateway
-     */
-    public function getVersion()
-    {
-        return self::$version;
-    }
-
-    /**
-     * Returns the name and URL for the authors of this gateway.
-     *
-     * @return array The name and URL of the authors of this gateway
-     */
-    public function getAuthors()
-    {
-        return self::$authors;
-    }
-
-    /**
-     * Return all currencies supported by this gateway.
-     *
-     * @return array A numerically indexed array containing all currency codes (ISO 4217 format) this gateway supports
-     */
-    public function getCurrencies()
-    {
-        return ['GBP'];
     }
 
     /**
